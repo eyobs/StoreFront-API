7608d41d756d3d4c70865d850fa28bff8923367c
eyobs <ttoeyob@gmail.com>
2022-02-24 15:21:10 +0300

    sending mails done!

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,14 @@
+from django.core.mail import send_mail, EmailMessage, BadHeaderError
 from django.shortcuts import render
-
+from templated_mail.mail import BaseEmailMessage
 
 def say_hello(request):
+    try:
+        message = BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={'name': 'Eyob'}
+        )
+        message.send(['john@ebuy.com'])
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Mosh'})
