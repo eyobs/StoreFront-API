31a550eed884a1ea9edd811fc482b0c2e4c67daf
eyobs <ttoeyob@gmail.com>
2022-06-16 17:04:05 +0300

    done up to caching

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,31 @@
+from urllib import response
+from django.core.cache import cache
 from django.shortcuts import render
-from .tasks import notify_customers
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework.views import APIView
+import requests
 
-def say_hello(request):
-    notify_customers.delay('hello')
-    return render(request, 'hello.html', {'name': 'Mosh'})
+
+class HelloView(APIView):
+    @method_decorator(cache_page(5 * 60))
+    def get(self, request):
+        response = requests.get('https://httpbin.org/delay/2')
+        data = response.json()
+        return render(request, 'hello.html', {'name': data})        
+
+
+# @cache_page(5 * 60)
+# def say_hello(request):
+#     response = requests.get('https://httpbin.org/delay/2')
+#     data = response.json()
+#     return render(request, 'hello.html', {'name': data})
+
+
+# def say_hello(request):
+#     key = 'httpbin_result'
+#     if cache.get(key) is None:
+#         response = requests.get('https://httpbin.org/delay/2')
+#         data = response.json()
+#         cache.set(key, data)
+#     return render(request, 'hello.html', {'name': cache.get(key)})
